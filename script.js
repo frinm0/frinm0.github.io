dc3964cd2dc9285af09e8b08cd994e9a5f79d12e
frinm0 <120174111+frinm0@users.noreply.github.com>
2023-03-14 16:18:54 +0500

    Add files via upload
    work

---
+++ script.js
@@ -0,0 +1,23 @@
+function func(){
+          var a = Number(document.getElementById("1").value);
+          var b = Number(document.getElementById("2").value);
+          var c = Number(document.getElementById("3").value);
+          var d = Number(document.getElementById("4").value);
+          var e = Number(document.getElementById("5").value);
+          var f = Number(document.getElementById("6").value);
+          var g = Number(document.getElementById("7").value);
+          var h = Number(document.getElementById("8").value);
+          var i = Number(document.getElementById("9").value);
+          var j = Number(document.getElementById("10").value);
+
+          var S = Number(document.getElementById("S").value);
+
+          var m
+          var V
+
+          m = (a+b+c+d+e+f+g+h+i+j)/10;
+          V = S * m;
+
+          document.getElementById("m").innerHTML = m + "    м";
+          document.getElementById("V").innerHTML = V + "    м<sup>3</sup>";
+      }
---
+++ script_2.js
@@ -0,0 +1,49 @@
+function func(){
+	let s1 = Number(document.getElementById("s1").value);
+    let s2 = Number(document.getElementById("s2").value);
+    let s3 = Number(document.getElementById("s3").value);
+    let l = Number(document.getElementById("l").value);
+    let v1
+    let v2
+    let e
+    let k
+    let sm
+    let vo
+    let vko
+
+    let h = Number(document.getElementById("h").value);
+    let vk1
+    let vk2
+    let result
+
+    sm= s1 * s2
+
+    k = sm ** 0.5
+
+    e = (s1 - s2) / s1
+
+    if (e <= 40) {
+    	v1 = ((s1 + s2) / 2) * l
+    	v2 = ((s2 + s3) / 2) * l
+    }
+    else if (e > 40) {
+    	v1 = (s1 * l + s2 * l + k * l) / 3
+    	v2 = (s2 * l + s3 * l + k * l) / 3
+    }
+    else {
+    	document.getElementById("result").innerHTML = "Ошибка";
+    }
+
+
+    
+    vk1 = 1/3 * s1 * h
+    vk2 = 1/3 * s3 * h
+    result = v1 + v2 + vk1 + vk2
+
+    vo = v1 + v2
+    vko = vk1 + vk2
+    document.getElementById("vo").innerHTML = "Объём: " + vo;
+    document.getElementById("vko").innerHTML = "Объём: " + vko;
+    document.getElementById("result").innerHTML = "Общий объём = " + result;
+}
+
